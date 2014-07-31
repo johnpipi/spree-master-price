203aa1a48cca4678ca3ed9e5f45bb48da608e240
John Peterson <johnpipi@gmail.com>
2014-07-31 10:41:03 -0700

    updated gem version

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
   Variant.class_eval do
-    attr_accessible :use_master_price
+    #attr_accessible :use_master_price
     def price
       return product.master.price if use_master_price? && !is_master?
       self[:price]
